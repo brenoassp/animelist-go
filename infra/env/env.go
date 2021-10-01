ac75781f7820f71b711d8ae55d09813afe73eb60
Breno Machado <breno_17@live.com>
2021-10-01 18:01:17 -0300

    add package to get env variabels

---
+++ infra/env/env.go
@@ -0,0 +1,30 @@
+package env
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func MustGetString(name string) string {
+	v := os.Getenv(name)
+	if v == "" {
+		fmt.Printf("environment variable %s was not found or it has an empty string", name)
+		os.Exit(1)
+	}
+	return v
+}
+
+func GetInt(name string, defaultValue ...int) int {
+	v, err := strconv.Atoi(os.Getenv(name))
+	if err != nil && len(defaultValue) == 0 {
+		fmt.Printf("environment variable %s was not found and no default value was provided", name)
+		os.Exit(1)
+	}
+
+	if err != nil && len(defaultValue) > 0 {
+		v = defaultValue[0]
+	}
+
+	return v
+}
